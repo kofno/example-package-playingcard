cdbd49c55b8bc506836ce6a1d216242284d3e1e3
kofno <ryan.l.bell@gmail.com>
2022-04-06 12:56:27 -0400

    Makes things comparable

--- Sources/PlayingCard/Suit.swift
+++ Sources/PlayingCard/Suit.swift
@@ -1,3 +1,25 @@
-public enum Suit: String {
+public enum Suit: String, CaseIterable {
   case spades, hearts, diamonds, clubs
+}
+
+extension Suit: Comparable {
+  public static func < (lhs: Suit, rhs: Suit) -> Bool {
+    switch (lhs, rhs) {
+    case (_, _) where lhs == rhs: return false
+    case (.spades, _), (.hearts, .diamonds), (.hearts, .clubs), (.diamonds, .clubs): return false
+    default:
+      return true
+    }
+  }
+}
+
+extension Suit: CustomStringConvertible {
+  public var description: String {
+    switch self {
+    case .spades: return "♠"
+    case .hearts: return "🤍"
+    case .diamonds: return "♦️"
+    case .clubs: return "♣️"
+    }
+  }
 }
